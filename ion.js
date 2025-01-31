869f665da8660932b86559a74bf3c17cfcd1114f
Sky <reeuuthdc80j@opayq.net>
2025-01-31 17:30:21 -0500

    Flesh out component feature

--- ion.js
+++ ion.js
@@ -1,28 +1,56 @@
 const fs = require('fs');
 
-fs.readFile('index.html', 'utf8', (err, data) => {
+var directoryPath = process.argv[2];
+fs.readdir(directoryPath, (err, files) => {
     if (err) {
-        console.error('Error reading the file:', err);
-        return;
+        return console.error('Unable to scan directory: ' + err);
     }
-    if (data.indexOf("<comp>") == -1) {return;}
-    else {
-        var tags = data.split(/(>)/);
-        for(let i = 0; i < tags.length; i++) {
-            if (tags[i].indexOf("</comp") != -1) {
-                
-                tags[i] = fs.readFileSync(`${tags[i].slice(0,-6)}`, 'utf8');
-                tags.splice(i-1, 1)
-                tags.splice(i-2, 1)
-                tags.splice(i-1, 1)
+    files.forEach(file => {
+        console.log(file)
+        fs.readFile(`${directoryPath}/${file}`, 'utf8', (err, data) => {
+            if (err) {
+                console.error('Error reading the file:', err);
+                return;
             }
-        }
-    }
-    fs.writeFile('output.html', tags.join("").replaceAll("\n", ""), (err) => {
+            if (data.indexOf("<comp>") == -1) {return;}
+            else {
+                var tags = data.split(/(>)/);
+                for(let i = 0; i < tags.length; i++) {
+                    if (tags[i].indexOf("</comp") != -1) {
+                        
+                        tags[i] = fs.readFileSync(`${directoryPath}/${tags[i].slice(0,-6)}`, 'utf8');
+                        tags.splice(i-1, 1)
+                        tags.splice(i-2, 1)
+                        tags.splice(i-1, 1)
+                    }
+                }
+            }
+
+            fs.access("output", fs.constants.F_OK, (err) => {
+                if (err) {
+                    fs.mkdir("output", { recursive: true }, (err) => {
+                        if (err) {
+                            console.error('Error creating directory:', err);
+                            return;
+                        }
+                        writeFile(file,tags);
+                    });
+
+                } 
+                else {
+                    writeFile(file,tags);
+                }
+            });
+        });
+    });
+});
+
+function writeFile(file,tags) {
+    fs.writeFile(`output/${file}`, tags.join("").replaceAll("\n", ""), (err) => {
         if (err) {
             console.error('Error writing to file:', err);
             return;
         }
     });
-});
+}
 
