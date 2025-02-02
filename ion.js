1410ac44400a09fdc5c5ddc51950e3d20aa21028
Sky <reeuuthdc80j@opayq.net>
2025-02-02 06:58:42 -0500

    Add basic import feature

--- ion.js
+++ ion.js
@@ -1,27 +1,28 @@
 const fs = require('fs');
-
-var directoryPath = process.argv[2];
-fs.readdir(directoryPath, (err, files) => {
-    if (err) {
-        return console.error('Unable to scan directory: ' + err);
-    }
+fs.readdir(process.argv[2], (err, files) => {
+    if (err) { return console.error('Unable to scan directory: ' + err); }
     files.forEach(file => {
-        console.log(file)
         fs.readFile(`${directoryPath}/${file}`, 'utf8', (err, data) => {
             if (err) {
                 console.error('Error reading the file:', err);
                 return;
             }
-            if (data.indexOf("<comp>") == -1) {return;}
+            if (data.indexOf("<comp>") == -1 && data.indexOf("<import>") == -1) {return;}
             else {
                 var tags = data.split(/(>)/);
                 for(let i = 0; i < tags.length; i++) {
                     if (tags[i].indexOf("</comp") != -1) {
-                        
                         tags[i] = fs.readFileSync(`${directoryPath}/${tags[i].slice(0,-6)}`, 'utf8');
+                        tags.splice(i-2, 2)
                         tags.splice(i-1, 1)
-                        tags.splice(i-2, 1)
+                    }
+                    if (tags[i].indexOf("</import") != -1) {
+                        var isCSS = tags[i].indexOf(".css") != -1 ? true : false;
+                        tags[i] = fs.readFileSync(`${directoryPath}/${tags[i].slice(0,-8)}`, 'utf8');
+                        tags.splice(i-2, 2)
                         tags.splice(i-1, 1)
+                        if (isCSS) { tags[i-2] = `<style>${tags[i-2]}</style>`; }
+                        else { tags[i-2] = `<script>${tags[i-2]}</script>`; }
                     }
                 }
             }
@@ -37,9 +38,7 @@ fs.readdir(directoryPath, (err, files) => {
                     });
 
                 } 
-                else {
-                    writeFile(file,tags);
-                }
+                else { writeFile(file,tags); }
             });
         });
     });
@@ -53,4 +52,3 @@ function writeFile(file,tags) {
         }
     });
 }
-
